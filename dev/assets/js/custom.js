648c09f44f2abdaec676354ccdd00c7e29632556
Your Name <you@example.com>
2025-05-11 16:22:25 +0300

    fear: creat swiper events

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -22,7 +22,7 @@ label.forEach(item => {
 
 const btn = document.querySelectorAll('.btn')
 
-const swiper = new Swiper('.swiper-product', {
+const swiperProduct = new Swiper('.swiper-product', {
     slidesPerView: 4,
     loop: true,
     speed: 500,
@@ -55,3 +55,24 @@ const swiper = new Swiper('.swiper-product', {
     }
    
   });
+
+  let swiperEvents;
+
+  function checkMediaQuery() {
+      if (window.matchMedia("(min-width: 1025px)").matches) {
+          swiperEvents?.destroy();
+          swiperEvents = null;
+      } else if (!swiperEvents) {
+          swiperEvents = new Swiper('.swiper-events', {
+              loop: true,
+              navigation: {
+                  nextEl: '.swiper-events-button-next',
+                  prevEl: '.swiper-events-button-prev',
+              },
+          });
+      }
+  }
+  
+  checkMediaQuery();
+  window.addEventListener('resize', checkMediaQuery);
+  window.addEventListener('orientationchange', () => setTimeout(checkMediaQuery, 300));
